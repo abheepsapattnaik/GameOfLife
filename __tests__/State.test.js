ff5ec2228e2f7a266143c55055ae89a7c229144e
Abheepsa Pattnaik <abheepsa.pattnaik@thoughtworks.com>
2018-11-09 11:56:59 +0530

    Add test for the dead and live with respect to neighbors

--- __tests__/State.test.js
+++ __tests__/State.test.js
@@ -9,4 +9,19 @@ describe('State of Cell', () => {
     const state = new State(DEAD);
     expect(state.isDead()).toBeTruthy();
   });
+  it('should change to live for a dead cell with 3 live neighbors', () => {
+    const state = new State(DEAD);
+    const updateState= state.updateStateForDead(3);
+    expect(updateState).toBe(LIVE);
+  });
+  it('should change to live for a live cell with 2 live neighbors', () => {
+    const state = new State(LIVE);
+    const updateState= state.updateStateForLive(2);
+    expect(updateState).toBe(LIVE);
+  });
+  it('should change to dead for a dead cell with 4 live neighbors', () => {
+    const state = new State(LIVE);
+    const updateState= state.updateStateForLive(4);
+    expect(updateState).toBe(DEAD);
+  });
 });
