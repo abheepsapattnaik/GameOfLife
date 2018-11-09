8f60a6e6f0dc7cef7a3183e59f87518b5d6427f2
Abheepsa Pattnaik <abheepsa.pattnaik@thoughtworks.com>
2018-11-09 12:57:07 +0530

    Add test for valid positions of the cell

--- __tests__/Cell.test.js
+++ __tests__/Cell.test.js
@@ -1,9 +1,10 @@
-import Cell from '../src/Cell';
-import {LIVE,DEAD} from "../src/Cell";
+import Cell,{LIVE,DEAD} from "../src/Cell";
+import Position from "../src/Position";
 
 describe('Cell', () => {
   it('should be a valid ', () => {
-    const cell = new Cell(1, 2, LIVE);
+    const position = new Position(1,2);
+    const cell = new Cell(position, LIVE);
     expect(cell.isValid()).toBeTruthy();
   });
   it('should be a invalid ', () => {
